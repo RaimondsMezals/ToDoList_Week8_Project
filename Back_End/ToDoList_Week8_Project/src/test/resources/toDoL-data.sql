f46be5f1e11e744f8b3e86492a17dc596d7eae38
RaimondsMezalsQA <RMezals@qa.com>
2021-02-18 12:13:11 +0000

    Merged front end with the back end

---
+++ Back_End/ToDoList_Week8_Project/src/test/resources/toDoL-data.sql
@@ -0,0 +1,4 @@
+insert into bag (listName) values ( 'ShoppingList');
+insert into bag (listName) values ( 'RandomItemsList');
+insert into bag (listName) values ( 'BasketList');
+insert into bag (listName) values ( 'ToolList');
