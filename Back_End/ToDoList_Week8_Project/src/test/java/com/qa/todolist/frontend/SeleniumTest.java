c92f3330289c44f50653998e19816bdbd1c593fd
RaimondsMezalsQA <RMezals@qa.com>
2021-02-20 22:28:24 +0000

    TDL-31 Added selenium tests

--- Back_End/ToDoList_Week8_Project/src/test/java/com/qa/todolist/frontend/SeleniumTest.java
+++ Back_End/ToDoList_Week8_Project/src/test/java/com/qa/todolist/frontend/SeleniumTest.java
@@ -1,5 +1,7 @@
 package com.qa.todolist.frontend;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,9 +11,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.qa.todolist.frontend.pom.SeleniumLinks;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
@@ -24,10 +30,12 @@ public class SeleniumTest {
 	private static ExtentReports report;
 	private static ExtentTest test;
 
+	SeleniumLinks website = PageFactory.initElements(driver, SeleniumLinks.class);
+
 	@BeforeAll
 	public static void setUp() {
 
-		report = new ExtentReports("./target/reports/extentreports/waitReport.html", true);
+		report = new ExtentReports("./target/reports/extentreports/frontendReport.html", true);
 
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 
@@ -45,6 +53,7 @@ public class SeleniumTest {
 	@BeforeEach
 	public void before() {
 		System.out.println("\nTest has started!");
+		driver.manage().window().maximize();
 	}
 
 	@AfterEach
@@ -58,22 +67,74 @@ public class SeleniumTest {
 	}
 
 	@Test
-	public void createBagTest() {
+	public void createTest() {
 
-		//
+		test = report.startTest("Selenium Test Create Bag");
+
 		test.log(LogStatus.INFO, "Given - I can access http://localhost:9090/index.html");
 		driver.get("http://localhost:9090/index.html");
 
 		test.log(LogStatus.INFO, "When - I navigate to the Bag Modification table");
-		targ = driver.findElement(By.xpath("/html/body/div/div[1]/div/div/div[1]/a/button"));
-		targ.click();
+		website.navigateBagModif();
 
 		test.log(LogStatus.INFO, "When - I enter values for a bag and crate a bag");
-		targ = driver.findElement(By.xpath("//*[@id=\"search_query_top\"]"));
-		targ.sendKeys("Dress");
+		targ = driver.findElement(By.xpath("//*[@id=\"listName\"]"));
+		targ.sendKeys("ShoppingList");
+		targ = driver.findElement(By.xpath("//*[@id=\"addListBtn\"]"));
+		targ.click();
 
 		test.log(LogStatus.INFO, "Then - I should see Successfully Created a (createTest) List");
+		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("addListPara")));
+		targ = driver.findElement(By.id("addListPara"));
+		String stringResult = targ.getText();
 
+		boolean result = stringResult.contains("Successfully Created a (ShoppingList) List");
+
+		if (result) {
+			test.log(LogStatus.PASS, "Successfully created a list!");
+		} else {
+			test.log(LogStatus.FAIL, "Failed to create" + stringResult);
+		}
+
+		assertEquals("Successfully Created a (ShoppingList) List", stringResult);
+
+		report.endTest(test);
+		report.flush();
+
+		test = report.startTest("Selenium Test Create Item");
+
+		test.log(LogStatus.INFO, "Given - I can access http://localhost:9090/index.html");
+
+		website.navigateHome();
+
+		test.log(LogStatus.INFO, "When - I navigate to the Item Modification table");
+		website.navigateItemModif();
+
+		test.log(LogStatus.INFO, "When - I enter values for an Item and crate an Item");
+		targ = driver.findElement(By.xpath("//*[@id=\"itemName\"]"));
+		targ.sendKeys("Cheese");
+		targ = driver.findElement(By.xpath("//*[@id=\"itemPrice\"]"));
+		targ.sendKeys("9.99");
+		targ = driver.findElement(By.xpath("//*[@id=\"bagId\"]"));
+		targ.sendKeys("1");
+
+		targ = driver.findElement(By.xpath("//*[@id=\"addListBtn\"]"));
+		targ.click();
+
+		test.log(LogStatus.INFO, "Then - I should see Successfully Created a (Cheese) Item");
+		new WebDriverWait(driver, 3).until(ExpectedConditions.presenceOfElementLocated(By.id("addListPara")));
+		targ = driver.findElement(By.id("addListPara"));
+		String stringResult2 = targ.getText();
+
+		boolean result2 = stringResult2.contains("Successfully Created a (Cheese) Item");
+
+		if (result2) {
+			test.log(LogStatus.PASS, "Successfully created an item!");
+		} else {
+			test.log(LogStatus.FAIL, "Failed to create" + stringResult2);
+		}
+
+		assertEquals("Successfully Created a (Cheese) Item", stringResult2);
 	}
 
 }
